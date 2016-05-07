c7c18e32eec4d3de60d91a7ad24f7d5e67530e41
Aram Kocharyan <akarmenia@gmail.com>
2016-05-08 01:42:13 +1000

    Version 0.3.0

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'urbanetic:bismuth-schema-utility',
   summary: 'Utilites for schemas and collections in Bismuth.',
   git: 'https://github.com/urbanetic/bismuth-schema-utility.git',
-  version: '0.2.1'
+  version: '0.3.0'
 });
 
 Package.on_use(function(api) {
@@ -13,17 +13,17 @@ Package.on_use(function(api) {
     'underscore',
     'aldeed:simple-schema@1.1.0',
     'aramk:requirejs@2.1.15_1',
-    'urbanetic:utility@1.0.1'
+    'urbanetic:utility@1.2.0'
   ], ['client', 'server']);
   api.use([
     'urbanetic:atlas@0.8.2'
   ], ['client', 'server'], {weak: true});
   api.addFiles([
     'src/ParamUtils.coffee',
-    'src/SchemaUtils.coffee',
+    'src/SchemaUtils.coffee'
   ], ['client', 'server']);
   api.export([
     'ParamUtils',
-    'SchemaUtils',
+    'SchemaUtils'
   ], ['client', 'server']);
 });
