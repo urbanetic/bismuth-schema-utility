13f5ba5bd0e047937db3e0fd8167f751241c7c4b
Aram Kocharyan <akocharyan@fb.com>
2018-02-26 00:49:33 -0800

    Meteor. 1.6.1 support

--- package.js
+++ package.js
@@ -2,18 +2,18 @@ Package.describe({
   name: 'urbanetic:bismuth-schema-utility',
   summary: 'Utilities for schemas and collections in Bismuth.',
   git: 'https://github.com/urbanetic/bismuth-schema-utility.git',
-  version: '0.3.0'
+  version: '1.0.0'
 });
 
 Package.on_use(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom('METEOR@1.6.1');
   api.use([
     'check',
     'coffeescript',
     'underscore',
     'aldeed:simple-schema@1.1.0',
     'aramk:requirejs@2.1.15_1',
-    'urbanetic:utility@1.2.0'
+    'urbanetic:utility@2.0.0'
   ], ['client', 'server']);
   api.use([
     'urbanetic:atlas@0.8.2'
